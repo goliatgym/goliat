ba857619407ec479e26d79cb1e69873c248878f5
Robert <robert.lapinski@outlook.com>
2015-10-11 22:54:58 +0200

    Initial commit

---
+++ Goliat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Goliat.Startup))]
+namespace Goliat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
